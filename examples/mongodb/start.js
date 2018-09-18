a0af41f478fc4a6ca9aef73fcbbcfbf9d299c3a2
zachary howe <vurtnec@users.noreply.github.com>
2018-09-18 11:21:28 +0800

    Update start.js

--- examples/mongodb/start.js
+++ examples/mongodb/start.js
@@ -12,3 +12,18 @@ server.on('error', function(err){
 server.on('ready', function(){
   console.log('Mosca server is up and running');
 });
+
+server.on('published', function(packet, client) {  
+  var topic = packet.topic; 
+  console.log(packet);  
+  switch(topic){  
+    case 'pubMsg':  
+      console.log('message-publish', packet.payload.toString());  
+      //MQTT转发主题消息  
+      MqttServer.publish({topic: 'other', payload: '12345678'});  
+      break;  
+    case 'other':  
+      console.log('message-123', packet.payload.toString());  
+      break;  
+  }  
+});  
