3719c56c92712c705561ac806f09d34cceab83e6
zachary howe <vurtnec@users.noreply.github.com>
2018-09-18 11:20:04 +0800

    Update config.js

--- examples/mongodb/config.js
+++ examples/mongodb/config.js
@@ -3,6 +3,11 @@ var mosca = require('../../');
 module.exports = {
   id: 'mymosca', // used to publish in the $SYS/<id> topicspace
   stats: false, // publish stats in the $SYS/<id> topicspace
+  port:1883,
+  interfaces: [
+    { type: "mqtt", port: 1883 },
+    { type: "http", port: 31113, bundle: true, static: './' }
+  ],
   logger: {
     level: 'debug'
   },
